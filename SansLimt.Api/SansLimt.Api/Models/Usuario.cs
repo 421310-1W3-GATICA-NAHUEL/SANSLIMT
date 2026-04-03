2f9c6c1ea02f1599deddb519098f5ec8c0f39c1f
Nahuel Gatica 421310 <421310@tecnicatura.frc.utn.edu.ar>
2026-04-03 19:26:27 -0300

    Primer Push

---
+++ SansLimt.Api/SansLimt.Api/Models/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SansLimt.Api.Models
+{
+    public class Usuario
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        
+        public string Rol { get; set; } = "Cliente";
+
+      
+        public string? Email { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? Telefono { get; set; }
+        public DireccionEnvio? Direccion { get; set; }
+    }
+
+    public class DireccionEnvio
+    {
+        public string? Calle { get; set; }
+        public string? Ciudad { get; set; }
+        public string? CodigoPostal { get; set; }
+    }
+}
